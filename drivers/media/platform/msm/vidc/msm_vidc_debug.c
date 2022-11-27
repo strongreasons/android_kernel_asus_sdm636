b156931bab2980bf75817e1808b7153208f839e2
Tiktodz <ewprjkt@proton.me>
2022-11-27 06:07:59 +0000

    drivers: msm: vidc: Silence debugfs failed errors
    × msm_vidc:  err: Failed to create debugfs for msm_vidc
    
    Change-Id: Ibfbff2ec6628d46e024d3c9858dbe78414887724

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -241,17 +241,17 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 	snprintf(debugfs_name, MAX_DEBUGFS_NAME, "core%d", core->id);
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (!dir) {
-		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
+//		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
 
 	if (!debugfs_create_file("info", S_IRUGO, dir, core, &core_info_fops)) {
-		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
+//		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
 		goto failed_create_dir;
 	}
 	if (!debugfs_create_file("trigger_ssr", S_IWUSR,
 			dir, core, &ssr_fops)) {
-		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
+//		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
 		goto failed_create_dir;
 	}
 failed_create_dir:
